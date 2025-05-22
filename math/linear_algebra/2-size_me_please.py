ec26248a702953fc35d1e42cfd0d7f7b0640f7c2
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-22 13:24:38 +0200

    ft: 2. Size Me Please

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns the shape of a matrix.
+"""
+
+
+def matrix_shape(matrix):
+  """return dimension of matrix"""
+  new_matrix = []
+  while type(matrix) == list:
+    new_matrix.append(len(matrix))
+    matrix = matrix[0]
+  return new_matrix
