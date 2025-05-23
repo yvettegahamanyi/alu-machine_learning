31ebcc81a4418eec603d52ec478dcc27e2317206
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-23 21:04:19 +0200

    ft: 3. Flip Me Over

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,9 +5,9 @@ This module contains a function that returns the shape of a matrix.
 
 
 def matrix_shape(matrix):
-  """return dimension of matrix"""
-  new_matrix = []
-  while type(matrix) == list:
-    new_matrix.append(len(matrix))
-    matrix = matrix[0]
-  return new_matrix
+    """return dimension of matrix"""
+    new_matrix = []
+    while type(matrix) == list:
+        new_matrix.append(len(matrix))
+        matrix = matrix[0]
+    return new_matrix
---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns the transpose of a matrix.
+"""
+
+
+def matrix_transpose(matrix):
+    """transpose a matrix"""
+  
+    row = len(matrix)
+    col = len(matrix[0])
+    new_matrix = []
+    for i in range(0, col):
+        aux_r = []
+        for j in range(0, row):
+            aux_r.append(matrix[j][i])
+        new_matrix.append(aux_r)
+    return new_matrix
