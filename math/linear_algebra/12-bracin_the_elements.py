e6c6deb12ece356132eb65daf70d20549c87a625
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-23 22:39:32 +0200

    ft:12. Bracing The Elements

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns transpose of array.
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """return some matrix operations"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
