30427826955b2aa881234a9f605311e14c6f07e0
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-23 22:20:46 +0200

    ft:9. Let The Butcher Slice It

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -13,8 +13,8 @@ def mat_mul(mat1, mat2):
         new_row = []
         for col in range(0, len(mat2[0])):
             new_value = 0
-        for value in range(0, len(mat1[0])):
-            new_value += mat1[row][value] * mat2[value][col]
-        new_row.append(new_value)
-    new_matrix.append(new_row)
+            for value in range(0, len(mat1[0])):
+                new_value += mat1[row][value] * mat2[value][col]
+            new_row.append(new_value)
+        new_matrix.append(new_row)
     return new_matrix
