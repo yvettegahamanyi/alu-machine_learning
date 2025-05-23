a7065146ba628858a9d5e82a218bc2bc1a6ef88a
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-23 22:09:35 +0200

    ft: 8. Ridin’ Bareback

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module contains a function multiply two arrays.
+"""
+
+
+def mat_mul(mat1, mat2):
+    """multiply two matrices"""
+    new_matrix = []
+    if len(mat1[0]) != len(mat2):
+        return None
+    for row in range(0, len(mat1)):
+        new_row = []
+        for col in range(0, len(mat2[0])):
+            new_value = 0
+        for value in range(0, len(mat1[0])):
+            new_value += mat1[row][value] * mat2[value][col]
+        new_row.append(new_value)
+    new_matrix.append(new_row)
+    return new_matrix
