0b4b2520dbe04a627614644fec4c80017df1a982
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-05-23 23:03:39 +0200

    ft:14. Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns dot product of array.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """multiply two matrices"""
+    return np.dot(mat1, mat2)
